d4a1de4b1fcb13059bd80014230c5907a20c0a02
sebastianorellana017 <sebastianorellana017@gmail.com>
2022-12-16 18:01:08 -0300

    debug static

--- config/urls.py
+++ config/urls.py
@@ -21,7 +21,4 @@ urlpatterns = [
     path("__reload__/", include("django_browser_reload.urls")),
 ]
 
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-else:
-    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
++ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
